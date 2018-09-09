2f558529212680309137d32f1de54f125a25fdb3
SScanry <pployukiss@gmail.com>
2018-09-09 17:55:28 +0700

    first commit

---
+++ mysqlconn.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "plearnja.com";
+$username = "plearnja_cpsu6";
+$password = "Lj5bU2bW";
+$dbname = "plearnja_cpsu6";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Set Charset
+  mysqli_set_charset($conn, "utf8");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
